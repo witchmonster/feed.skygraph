9eb32a05a454c28fd2fbd8f01d14a3e723462273
mathan <yuliya.kramarenko@gmail.com>
2024-05-22 13:54:15 -0700

    Update skygraph to prod.

--- src/algos/feeds/skygraph.ts
+++ src/algos/feeds/skygraph.ts
@@ -2,6 +2,7 @@ import { InvalidRequestError } from '@atproto/xrpc-server'
 import { QueryParams } from '../../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import { AppContext } from '../../config'
 import { sql } from 'kysely'
+import { getUserCommunity } from '../common/communities'
 
 // max 15 chars
 export const shortname = 'skygraph'
@@ -18,30 +19,6 @@ function shuffleArray(array) {
 export const handler = async (ctx: AppContext, params: QueryParams, userDid: string) => {
     console.log(userDid);
 
-    const communitiesRes = await sql`select f, s, c, g, e, o from did_to_community where did = ${userDid ? userDid : 'did:plc:v7iswx544swf2usdcp32p647'}`.execute(ctx.db);
-    const communityCodes: string[] = [
-        (communitiesRes.rows[0] as any)?.f,
-        (communitiesRes.rows[0] as any)?.s,
-        (communitiesRes.rows[0] as any)?.c,
-        (communitiesRes.rows[0] as any)?.g,
-        (communitiesRes.rows[0] as any)?.e,
-        (communitiesRes.rows[0] as any)?.o];
-
-
-    const communities = await ctx.db.selectFrom('community')
-        .selectAll()
-        .where('community', 'in', communityCodes)
-        .orderBy('community asc')
-        .execute();
-
-    console.log(communities);
-
-    const perfectCommunity = (communities && communities.filter(community => community.size > 3000 && community.size < 50000)[0])
-        ?? (communities && communities.filter(community => community.prefix === 's'))[0];
-
-    console.log(perfectCommunity);
-    const whereClause: any = `did_to_community.${perfectCommunity.prefix}`;
-
     let seed: number;
     let existingCid;
     if (params.cursor) {
@@ -57,30 +34,6 @@ export const handler = async (ctx: AppContext, params: QueryParams, userDid: str
 
     console.log(`${seed}::${existingCid}`);
 
-    const topLikedAuthors = ctx.db.selectFrom('likescore')
-        .innerJoin('did_to_community', 'likescore.subject', 'did_to_community.did')
-        .select(['did_to_community.e', 'likescore.subject'])
-        .where('likescore.author', '=', userDid)
-        .where(whereClause, '<>', perfectCommunity.community)
-        .limit(5);
-
-    console.log(topLikedAuthors.compile().sql);
-
-    const topLikedConstellations = await topLikedAuthors.execute();
-
-    console.log("top nebulae:" + topLikedConstellations?.map(n => n.e));
-
-    // let exploreNebulae;
-    // if (topLikedNebulae && topLikedNebulae.length > 0) {
-    //     exploreNebulae = await ctx.db.selectFrom('likescore')
-    //         .innerJoin('did_to_community', 'likescore.subject', 'did_to_community.did')
-    //         .select(['did_to_community.e'])
-    //         .where('likescore.author', 'in', topLikedNebulae.map(n => n.subject))
-    //         .where(whereClause, '<>', perfectCommunity.community)
-    //         .limit(5)
-    //         .execute();
-    // }
-
     let builder = ctx.db
         .selectFrom('post')
         .selectAll()
@@ -90,23 +43,22 @@ export const handler = async (ctx: AppContext, params: QueryParams, userDid: str
             // if the post is popular (>50 likes) there's 70% chance it will get downranked to 10 likes so you don't see the same top liked post on top all the time
             sql<string>`((postrank.score-1)*(case when score > 50 and rand(${seed}) > 0.6 then 1 else 10/(score-1) end)*rand(${seed})/power(timestampdiff(second,post.indexedAt,now())/3600 + 2,2))`.as('rank')
         ])
-        .innerJoin('did_to_community', 'post.author', 'did_to_community.did')
         .innerJoin('postrank', 'post.uri', 'postrank.uri')
         // .where('post.replyParent', 'is', null)
         .orderBy('rank', 'desc')
         .limit(params.limit);
 
-    if (topLikedConstellations && topLikedConstellations.length > 0) {
-        const nebulaCodes = topLikedConstellations.map(n => n.e);
+    const { whereClause, userCommunity: community, topConstellationsByLikes } = await getUserCommunity(ctx, userDid, { withTopLiked: true });
+
+    if (topConstellationsByLikes && topConstellationsByLikes.length > 0) {
         builder = builder
             .where((eb) => eb.or([
-                eb('did_to_community.e', 'in', nebulaCodes),
-                // eb('did_to_community.e', 'in', exploreNebulae ?? []),
-                eb(whereClause, '=', perfectCommunity.community),
+                eb('post.o', 'in', topConstellationsByLikes),
+                eb(whereClause, '=', community),
             ]))
     } else {
         builder = builder
-            .where(whereClause, '=', perfectCommunity.community)
+            .where(whereClause, '=', community)
     }
 
     if (existingCid) {
