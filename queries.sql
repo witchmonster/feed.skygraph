a4f6c96901cca556840dd3f46390f87552858d80
mathan <yuliya.kramarenko@gmail.com>
2024-05-30 09:38:09 -0700

    Updated low post output debug query.

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 select count(distinct user) from feed_usage where feed_usage.limit=30 and (feed='skygraph' or feed='nebula_plus') and lastUpdated > DATE_SUB(NOW(), INTERVAL 2 day) order by refreshcount desc;
 
 -- debug low post output users
-select * from feed_usage where last_post_output is not null and feed_usage.limit=30 order by last_post_output asc limit 20;
+select * from feed_usage where last_post_output is not null and feed_usage.limit>=10 and lastUpdated > DATE_SUB(NOW(), INTERVAL 1 hour) and last_post_output < feed_usage.limit / 2 order by last_post_output asc limit 20;
 
 -- debug user nebulas
 select sum(l.score), dc.e from likescore l join did_to_community dc on l.subject=dc.did where l.author = 'did:plc:abcd...' group by dc.e order by sum(l.score) desc;
