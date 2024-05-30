30808ec6479fd9fb944b238a18775f0a0984a461
mathan <yuliya.kramarenko@gmail.com>
2024-05-30 14:25:35 -0700

    Update queries.

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 
 -- count unique users who loaded the feed
-select count(distinct user) from feed_usage where feed_usage.limit=30 and (feed='skygraph' or feed='nebula_plus') and lastUpdated > DATE_SUB(NOW(), INTERVAL 2 day) order by refreshcount desc;
+select count(distinct user) from feed_usage where feed_usage.limit>=10 and (feed='skygraph' or feed='nebula_plus') and lastUpdated > DATE_SUB(NOW(), INTERVAL 7 day) order by refreshcount desc;
 
 -- debug low post output users
 select * from feed_usage where last_post_output is not null and feed_usage.limit>=10 and lastUpdated > DATE_SUB(NOW(), INTERVAL 1 hour) and last_post_output < feed_usage.limit / 2 order by last_post_output asc limit 20;
